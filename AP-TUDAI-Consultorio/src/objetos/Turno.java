f3be621c1964c2293aa4a13094aba4f911b56ace
Omarit82 <roselliomar82@gmail.com>
2025-12-01 11:41:20 -0300

    Feature/Confirmacion de turnos

--- AP-TUDAI-Consultorio/src/objetos/Turno.java
+++ AP-TUDAI-Consultorio/src/objetos/Turno.java
@@ -10,6 +10,9 @@ public class Turno {
     private Profesional profesional;
     private LocalDate fecha;
     private Hora hora;
+    private boolean asistencia;
+    private int turnoId;
+    private static int contador=1;
 
     //CONSTRUCTOR
     public Turno(Puesto puestoAsignado, Paciente paciente, Profesional profesional, LocalDate fecha,Hora hora) {
@@ -18,6 +21,9 @@ public class Turno {
         this.profesional = profesional;
         this.fecha = fecha;
         this.hora = hora;
+        this.asistencia = false;
+        this.turnoId = contador;
+        this.contador++;
     }
     //Getters & Setters
 
@@ -71,9 +77,30 @@ public class Turno {
     public void setHora(Hora hora) {
         this.hora = hora;
     }
+    
+
+    public int getTurnoId() {
+        return turnoId;
+    }
+
+    public void setTurnoId(int turnoId) {
+        this.turnoId = turnoId;
+    }
+    
+    public boolean isAsistencia() {
+        return asistencia;
+    }
+
+    public void setAsistencia(boolean asistencia) {
+        this.asistencia = asistencia;
+    }
+
+    public void pacienteAsistio(){
+        paciente.setSesionesRemanentes(paciente.getSesionesRemanentes()-1);
+    }
 
     @Override
     public String toString(){
-        return "Turno: "+fecha.toString()+"\nPaciente:"+paciente.getApellido()+" Profesional: "+profesional.getApellido()+"Puesto: "+puestoAsignado.getNombre();
+        return "Turno: "+fecha.toString()+" Hora:"+hora+"\nPaciente:"+paciente.getApellido()+" Profesional: "+profesional.getApellido()+" Puesto: "+puestoAsignado.getNombre();
     }
 }
